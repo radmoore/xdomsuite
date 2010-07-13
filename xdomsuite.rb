2d709e92125073a502633a736e231ff66d7a55c3
Diplom-Bio Andrew Moore <radmoore@vebb.uni-muenster.de>
2010-07-13 12:02:14 +0200

    Revert "* testing merge again"
    This reverts commit b0c916ab0c3d6d7b8d7ea818cb036b218bfe176f.

--- xdomsuite.rb
+++ xdomsuite.rb
@@ -68,7 +68,6 @@ class Parser
 
   # Returns an instance of the Parser class.
   def initialize(filename)
-		@testatt = "this is different from local"
     @filename = filename
     @file_ext = File.extname(@filename)
     @file_bn = File.basename(@filename, @file_ext)
